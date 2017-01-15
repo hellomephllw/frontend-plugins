709477683d3f052c7afc729f92aba28841f90499
hellomephllw <hellomephllw@163.com>
2017-01-16 02:09:38 +0800

    finish the index control

--- plugins/by-jq/carousel/demo1/index.js
+++ plugins/by-jq/carousel/demo1/index.js
@@ -17,9 +17,12 @@
         autoMoveInterval: null,
         //定时器速度
         intervalSpeed: 3000,
+        //移动速度
+        moveSpeed: 0.5,
         //正在移动
         isMove: false,
         isReplace: false,
+        isReplaceImage: false,
         isMouseIn: false,
         /**jq节点*/
         $carousel: $('#carousel'),
@@ -41,7 +44,7 @@
             this.autoMove();
             this.stopAutoMove();
             this.continueAutoMove();
-            // this.clickIndexPoint();
+            this.clickIndexPoint();
         },
         /**生成图片节点*/
         ganeratePicNodes: function () {
@@ -84,8 +87,8 @@
         },
         /**记录数据：一些数据需要做即时记录*/
         record: function(currentIndex, translateX) {
-            caches.currentIndex = currentIndex;
-            caches.currentTranslateXValue = translateX;
+            caches.currentIndex = Math.round(currentIndex);
+            caches.currentTranslateXValue = Math.round(translateX);
         },
         /**自动移动*/
         autoMove: function () {
@@ -95,8 +98,13 @@
         },
         /**移动到右图*/
         moveToRightPicture: function() {
-            //
-            // caches.isMove = true;
+            //标记正在移动
+            caches.isMove = true;
+            var isMoveTimeout = setTimeout(function() {
+                //取消正在移动标记
+                caches.isMove = false;
+                clearTimeout(isMoveTimeout);
+            }, caches.moveSpeed * 1000);
             //记录当前位置信息
             this.record(caches.currentIndex + 1, caches.currentTranslateXValue - caches.screenWidth);
             //移动到右图
@@ -108,7 +116,9 @@
 
             //如果移动到了最后一张图片
             if (caches.currentIndex === caches.url.length + 1) {
-                caches.isReplace = true;
+                //正在替换，即偷梁换柱
+                caches.isReplace = true;//大步骤
+                caches.isReplaceImage = true;//图片替换小步骤
                 //停止自动移动
                 clearInterval(caches.autoMoveInterval);
                 //偷梁换柱
@@ -120,13 +130,19 @@
                         transition: 'none',
                         transform: carousel.makeTranslate(caches.currentTranslateXValue)
                     });
-                    //还原过度
+                    //还原transition
                     var restoreTransitionTimeout = setTimeout(function() {
-                        caches.$carouselPicList.css({
-                            transition: 'transform 0.5s linear'
-                        });
                         clearTimeout(restoreTranslateTimeout);
                         clearTimeout(restoreTransitionTimeout);
+                        caches.$carouselPicList.css({
+                            transition: 'transform ' + caches.moveSpeed + 's linear'
+                        });
+                    }, 1);
+                    //完成图片替换
+                    caches.isReplaceImage = false;
+                    //替换后再次移动
+                    var afterReplaceMoveTimeout = setTimeout(function() {
+                        clearTimeout(afterReplaceMoveTimeout);
                         //鼠标移入禁止移动
                         if (caches.isMouseIn) {
                             //替换完成
@@ -139,8 +155,8 @@
                         carousel.autoMove();
                         //替换完成
                         caches.isReplace = false;
-                    }, 1);
-                }, caches.intervalSpeed);
+                    }, Math.abs(caches.intervalSpeed - caches.moveSpeed * 1000));
+                }, caches.moveSpeed * 1000 + 100);
             }
         },
         /**生成索引*/
@@ -188,8 +204,24 @@
         /**点击索引*/
         clickIndexPoint: function() {
             caches.$carouselIndexList.on('click', 'li', function(event) {
-                 caches.currentIndex = $(event.target).attr('data-index');
-                 carousel.changeActiveIndexPoint();
+                //正在移动不允许点击
+                if (caches.isMove) return ;
+                //正在替换图片不允许点击
+                if (caches.isReplaceImage) return ;
+                //获取索引值
+                var currentIndex = $(event.target).attr('data-index');
+                //记录
+                carousel.record(currentIndex, -currentIndex * caches.screenWidth);
+                //改变索引
+                carousel.changeActiveIndexPoint();
+                //改变图片
+                carousel.changeToAppointPic();
+            });
+        },
+        /**改变到指定图片*/
+        changeToAppointPic: function () {
+            caches.$carouselPicList.css({
+                transform: carousel.makeTranslate(caches.currentTranslateXValue)
             });
         }
     };
